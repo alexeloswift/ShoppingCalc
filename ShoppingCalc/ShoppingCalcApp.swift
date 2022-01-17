f23ad10b59eb83bc07a15362b497792dbbee31a7
Alex Diaz <alexelo.swift@gmail.com>
2022-01-17 00:25:13 -0500

    Initial Commit

---
+++ ShoppingCalc/ShoppingCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCalcApp.swift
+//  ShoppingCalc
+//
+//  Created by Alexis Diaz on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
